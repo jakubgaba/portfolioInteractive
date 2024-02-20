7e37f10ba2f4b1481977503f1f22b2bc6058b9a4
Jakub <j.gaba@email.cz>
2024-02-20 13:18:52 +0100

    News

--- Experience/World/Controls.js
+++ Experience/World/Controls.js
@@ -88,7 +88,7 @@ export default class Controls {
     setScrollTrigger() {
         ScrollTrigger.matchMedia({
             //Desktop
-            "(max-width: 969px)": () => {
+            "(min-width: 969px)": () => {
                 console.log("fired desktop");
 
                 this.room.scale.set(0.11, 0.11, 0.11);
@@ -173,7 +173,7 @@ export default class Controls {
             },
 
             // Mobile
-            "(min-width: 968px)": () => {
+            "(max-width: 968px)": () => {
                 console.log("fired mobile");
 
                 // Resets
